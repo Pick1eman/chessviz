d763a6f24d84c4c2d2a8f60f9044614849f8b3c1
Кожуховский Геннадий Николаевич <is741s14@mwp12.csc.local>
2018-04-26 08:59:20 +0700

    Add function print_board() + fix board.c

--- board.c
+++ board.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include "board.h"
 
-char checknum(int num_figure)
+extern unsigned short int board[8][8];
+
+//extern short ch_board[8][8];
+
+char checknum(unsigned short int num_figure)
 {
 		/*
 	none = 0
@@ -47,27 +51,68 @@ char checknum(int num_figure)
 		case 15:
 			return 'K';
 	}
+}
 
 
+char code_to_letter(unsigned short int i1, unsigned short int i2)
+{
+	char empty = '!';
+	if ((i + j)%2 != 0) {
+		empty = '#';
+	} else {
+		empty = ' ';
+	}
+	
+	switch(board[i1][j1]){
+		case 21:
+			return 'r';
+		case 22:
+			return 'n';
+		case 23:
+			return 'b';
+		case 24:
+			return 'q';
+		case 25:
+			return 'k';
+		case 20:
+			return 'p';
+		case 10:
+			return 'P';
+		case 11:
+			return 'R';
+		case 12:
+			return 'N';
+		case 13:
+			return 'B';
+		case 14:
+			return 'Q';
+		case 15:
+			return 'K';
+		case 0:
+			return empty;
+	}
+}
 
 
-char Ini_cells(int i1, int j1)
+char Ini_cells(unsigned short int i1,unsigned short int j1)
 {
 	char figure;
-	int num_figure;
-	int i2, j2;
+	unsigned short int num_figure;
+	char move[2];
 	/*unsigned short int board[8][8] = {{21, 22, 23, 24, 25, 23, 22, 21},
-									  {20},
-									  {0},
-									  {0},
-									  {0},
-									  {0},
-									  {10},
+									  {20, 20, 20, 20, 20, 20, 20, 20},
+									  {0, 0, 0, 0, 0, 0, 0, 0},
+									  {0, 0, 0, 0, 0, 0, 0, 0},
+									  {0, 0, 0, 0, 0, 0, 0, 0},
+									  {0, 0, 0, 0, 0, 0, 0, 0},
+									  {10, 10, 10, 10, 10, 10, 10, 10},
 									  {11, 12, 13, 14, 15, 13, 12, 11}};*/
 	num_figure = board[i1][j1];
 	if (num_fugure == 21) {
 		printf("Введите координату,куда хотите переместить ладью: ");
-		scanf("%d %d", &i2, &j2);
+		scanf("%c %c", &move[0], &move[1]);
+		i2 = table_1(move[1]);
+		j2 = table_2(move[0]);
 		board[i2][j2] = num_figure;//num_figure = board[i2][j2];
 		board[i1][j1] = 0;
 		figure = checknum(num_figure);
@@ -75,7 +120,9 @@ char Ini_cells(int i1, int j1)
 	}
 		if (num_fugure == 22) {
 		printf("Введите координату,куда хотите переместить коня: ");
-		scanf("%d %d", &i2, &j2);
+		scanf("%c %c", &move[0], &move[1]);
+		i2 = table_1(move[1]);
+		j2 = table_2(move[0]);
 		board[i2][j2] = num_figure;//num_figure = board[i2][j2];
 		board[i1][j1] = 0;
 		figure = checknum(num_figure);
@@ -83,7 +130,9 @@ char Ini_cells(int i1, int j1)
 	}
 		if (num_fugure == 23) {
 		printf("Введите координату,куда хотите переместить слона: ");
-		scanf("%d %d", &i2, &j2);
+		scanf("%c %c", &move[0], &move[1]);
+		i2 = table_1(move[1]);
+		j2 = table_2(move[0]);
 		board[i2][j2] = num_figure;//num_figure = board[i2][j2];
 		board[i1][j1] = 0;
 		figure = checknum(num_figure);
@@ -91,7 +140,9 @@ char Ini_cells(int i1, int j1)
 	}
 		if (num_fugure == 24) {
 		printf("Введите координату,куда хотите переместить ферзя: ");
-		scanf("%d %d", &i2, &j2);
+		scanf("%c %c", &move[0], &move[1]);
+		i2 = table_1(move[1]);
+		j2 = table_2(move[0]);
 		board[i2][j2] = num_figure;//num_figure = board[i2][j2];
 		board[i1][j1] = 0;
 		figure = checknum(num_figure);
@@ -99,7 +150,9 @@ char Ini_cells(int i1, int j1)
 	}
 		if (num_fugure == 25) {
 		printf("Введите координату,куда хотите переместить короля: ");
-		scanf("%d %d", &i2, &j2);
+		scanf("%c %c", &move[0], &move[1]);
+		i2 = table_1(move[1]);
+		j2 = table_2(move[0]);
 		board[i2][j2] = num_figure;//num_figure = board[i2][j2];
 		board[i1][j1] = 0;
 		figure = checknum(num_figure);
@@ -107,7 +160,9 @@ char Ini_cells(int i1, int j1)
 	}
 		if (num_fugure == 20) {
 		printf("Введите координату,куда хотите переместить пешку: ");
-		scanf("%d %d", &i2, &j2);
+		scanf("%c %c", &move[0], &move[1]);
+		i2 = table_1(move[1]);
+		j2 = table_2(move[0]);
 		board[i2][j2] = num_figure;//num_figure = board[i2][j2];
 		board[i1][j1] = 0;
 		figure = checknum(num_figure);
@@ -115,7 +170,9 @@ char Ini_cells(int i1, int j1)
 	}
 		if (num_fugure == 11) {
 		printf("Введите координату,куда хотите переместить ладью: ");
-		scanf("%d %d", &i2, &j2);
+		scanf("%c %c", &move[0], &move[1]);
+		i2 = table_1(move[1]);
+		j2 = table_2(move[0]);
 		board[i2][j2] = num_figure;//num_figure = board[i2][j2];
 		board[i1][j1] = 0;
 		figure = checknum(num_figure);
@@ -123,7 +180,9 @@ char Ini_cells(int i1, int j1)
 	}
 		if (num_fugure == 12) {
 		printf("Введите координату,куда хотите переместить коня: ");
-		scanf("%d %d", &i2, &j2);
+		scanf("%c %c", &move[0], &move[1]);
+		i2 = table_1(move[1]);
+		j2 = table_2(move[0]);
 		board[i2][j2] = num_figure;//num_figure = board[i2][j2];
 		board[i1][j1] = 0;
 		figure = checknum(num_figure);
@@ -131,14 +190,18 @@ char Ini_cells(int i1, int j1)
 	}
 		if (num_fugure == 13) {
 		printf("Введите координату,куда хотите переместить слона: ");
-		scanf("%d %d", &i2, &j2);
+		scanf("%c %c", &move[0], &move[1]);
+		i2 = table_1(move[1]);
+		j2 = table_2(move[0]);
 		board[i2][j2] = num_figure;//num_figure = board[i2][j2];
 		board[i1][j1] = 0;
 		figure = checknum(num_figure);
 		return figure;
 	}	if (num_fugure == 14) {
 		printf("Введите координату,куда хотите переместить ферзя: ");
-		scanf("%d %d", &i2, &j2);
+		scanf("%c %c", &move[0], &move[1]);
+		i2 = table_1(move[1]);
+		j2 = table_2(move[0]);
 		board[i2][j2] = num_figure;//num_figure = board[i2][j2];
 		board[i1][j1] = 0;
 		figure = checknum(num_figure);
@@ -146,7 +209,9 @@ char Ini_cells(int i1, int j1)
 	}
 		if (num_fugure == 15) {
 		printf("Введите координату,куда хотите переместить короля: ");
-		scanf("%d %d", &i2, &j2);
+		scanf("%c %c", &move[0], &move[1]);
+		i2 = table_1(move[1]);
+		j2 = table_2(move[0]);
 		board[i2][j2] = num_figure;//num_figure = board[i2][j2];
 		board[i1][j1] = 0;
 		figure = checknum(num_figure);
@@ -154,7 +219,9 @@ char Ini_cells(int i1, int j1)
 	}
 		if (num_fugure == 10) {
 		printf("Введите координату,куда хотите переместить пешку: ");
-		scanf("%d %d", &i2, &j2);
+		scanf("%c %c", &move[0], &move[1]);
+		i2 = table_1(move[1]);
+		j2 = table_2(move[0]);
 		board[i2][j2] = num_figure;//num_figure = board[i2][j2];
 		board[i1][j1] = 0;
 		figure = checknum(num_figure);
@@ -164,55 +231,50 @@ char Ini_cells(int i1, int j1)
 }
 
 
-void board()
+void board_(unsigned short int i1,unsigned short int j1)
 {
-	
-		/*
-	none = 0
-	
-	wp = 10
-	wr = 11
-	wn = 12
-	wb = 13
-	wq = 14
-	wk = 15
-	
-	bp = 20
-	br = 21
-	bn = 22
-	bb = 23
-	bq = 24
-	bk = 25
-	*/
+
 	char figure; 
-	char move[4] = {'e', '2', 'e', '4'};
-	unsigned short int board[8][8] = {{21, 22, 23, 24, 25, 23, 22, 21},
-									  {20},
-									  {0},
-									  {0},
-									  {0},
-									  {0},
-									  {10},
-									  {11, 12, 13, 14, 15, 13, 12, 11}};
 	
+	
+	figure = Ini_cells(i1, j1);//Для перемещения фигуры
+	
+
+	
+}
+
+
+
+void print_board(unsigned short int i1,unsigned short int j1)
+{
+	int num_figure;
+	num_figure = board[i1][j1];
 	printf("\n\n");
 	printf("    *  | A | B | C | D | E | F | G | H |  *    \n");
 	printf("    _   ___ ___ ___ ___ ___ ___ ___ ___   _    \n");
-	printf("    8  | %c |#%c#| %c |#%c#| %c |#%c#| %c |#%c#|  8    \n");
+	printf("    8  | %c |#%c#| %c |#%c#| %c |#%c#| %c |#%c#|  8    \n", 
+	code_to_letter(0,0), code_to_letter(0,1),code_to_letter(0,2),code_to_letter(0,3),code_to_letter(0,4),code_to_letter(0,5),code_to_letter(0,6),code_to_letter(0,7));
 	printf("    _  |___|###|___|###|___|###|___|###|  _    \n");
-	printf("    7  |#%c#| %c |#%c#| %c |#%c#| %c |#%c#| %c |  7    \n");
+	printf("    7  |#%c#| %c |#%c#| %c |#%c#| %c |#%c#| %c |  7    \n",
+	code_to_letter(1,0), code_to_letter(1,1),code_to_letter(1,2),code_to_letter(1,3),code_to_letter(1,4),code_to_letter(1,5),code_to_letter(1,6),code_to_letter(1,7));
 	printf("    _  |###|___|###|___|###|___|###|___|  _    \n");
-	printf("    6  | %c |#%c#| %c |#%c#| %c |#%c#| %c |#%c#|  6    \n");
+	printf("    6  | %c |#%c#| %c |#%c#| %c |#%c#| %c |#%c#|  6    \n",
+	code_to_letter(2,0), code_to_letter(2,1),code_to_letter(2,2),code_to_letter(2,3),code_to_letter(2,4),code_to_letter(2,5),code_to_letter(2,6),code_to_letter(2,7));
 	printf("    _  |___|###|___|###|___|###|___|###|  _    \n");
-	printf("    5  |#%c#| %c |#%c#| %c |#%c#| %c |#%c#| %c |  5    \n");
+	printf("    5  |#%c#| %c |#%c#| %c |#%c#| %c |#%c#| %c |  5    \n",
+	code_to_letter(3,0), code_to_letter(3,1),code_to_letter(3,2),code_to_letter(3,3),code_to_letter(3,4),code_to_letter(3,5),code_to_letter(3,6),code_to_letter(3,7));
 	printf("    _  |###|___|###|___|###|___|###|___|  _    \n");
-	printf("    4  | %c |#%c#| %c |#%c#| %c |#%c#| %c |#%c#|  4    \n");
+	printf("    4  | %c |#%c#| %c |#%c#| %c |#%c#| %c |#%c#|  4    \n",
+	code_to_letter(4,0), code_to_letter(4,1),code_to_letter(4,2),code_to_letter(4,3),code_to_letter(4,4),code_to_letter(4,5),code_to_letter(4,6),code_to_letter(4,7));
 	printf("    _  |___|###|___|###|___|###|___|###|  _    \n");
-	printf("    3  |#%c#| %c |#%c#| %c |#%c#| %c |#%c#| %c |  3    \n");
+	printf("    3  |#%c#| %c |#%c#| %c |#%c#| %c |#%c#| %c |  3    \n",
+	code_to_letter(5,0), code_to_letter(5,1),code_to_letter(5,2),code_to_letter(5,3),code_to_letter(5,4),code_to_letter(5,5),code_to_letter(5,6),code_to_letter(5,7));
 	printf("    _  |###|___|###|___|###|___|###|___|  _    \n");
-	printf("    2  | %c |#%c#| %c |#%c#| %c |#%c#| %c |#%c#|  2    \n");
+	printf("    2  | %c |#%c#| %c |#%c#| %c |#%c#| %c |#%c#|  2    \n",
+	code_to_letter(6,0), code_to_letter(6,1),code_to_letter(6,2),code_to_letter(6,3),code_to_letter(6,4),code_to_letter(6,5),code_to_letter(6,6),code_to_letter(6,7));
 	printf("    _  |___|###|___|###|___|###|___|###|  _    \n");
-	printf("    1  |#%c#| %c |#%c#| %c |#%c#| %c |#%c#| %c |  1    \n");
+	printf("    1  |#%c#| %c |#%c#| %c |#%c#| %c |#%c#| %c |  1    \n",
+	code_to_letter(7,0), code_to_letter(7,1),code_to_letter(7,2),code_to_letter(7,3),code_to_letter(7,4),code_to_letter(7,5),code_to_letter(7,6),code_to_letter(7,7));
 	printf("    _  |###|___|###|___|###|___|###|___|  _    \n");
 	printf("                                               \n");
 	printf("    *  | A | B | C | D | E | F | G | H |  *    \n");
@@ -220,7 +282,7 @@ void board()
 	
 }
 
-int table_1(char *a)
+unsigned short int table_1(char *a)//Преобразование символа в число
 {
 		switch(*a){
 		case 'a':
@@ -239,30 +301,30 @@ int table_1(char *a)
 			return 6;
 		case 'h':
 			return 7;
-		{
-	}
+		}
+}
 	
 	
 	
 	
-int table_2(char *b)
+unsigned short int table_2(char *b)//Преобразование символа в число
 {	
 		switch(*b){
 		case '1':
-			return 0;
-		case '2':
-			return 1;
-		case '3':
-			return 2;
-		case '4':
-			return 3;
-		case '5':
-			return 4;
-		case '6':
-			return 5;
-		case '7':
-			return 6;
-		case '8':
 			return 7;
+		case '2':
+			return 6;
+		case '3':
+			return 5;
+		case '4':
+			return 4;
+		case '5':
+			return 3;
+		case '6':
+			return 2;
+		case '7':
+			return 1;
+		case '8':
+			return 0;
 		}
 }
